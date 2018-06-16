65b1855f08a6b4156a093b65ae96a25dba2dfc5e
Erik Burnell <erik.d.burnell@gmail.com>
2018-06-16 18:02:38 +0200

    Moved Game to Home Controller.

--- CRAMWeb/CRAMWeb/Controllers/HomeController.cs
+++ CRAMWeb/CRAMWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRAMWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,16 @@ namespace CRAMWeb.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        // GET: Game
+        public ActionResult Game()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var users = db.Users.ToList();
+            }
             return View();
         }
 
